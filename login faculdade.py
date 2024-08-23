26443d65f272b254a9f65544e1c79a49da1a61e2
Vitor1017 <vitor.alves.miranda@hotmail.com>
2024-08-22 22:03:11 -0300

    Add files via upload

---
+++ login faculdade.py
@@ -0,0 +1,52 @@
+import pyautogui                    
+import subprocess
+import time
+
+url_site = "https://www.colaboraread.com.br/login/auth" 
+
+# Abrir o Chrome diretamente com a URL desejada
+
+subprocess.run(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+
+time.sleep(2)  # Espera o navegador abrir e a página carregar
+pyautogui.write(url_site)
+
+time.sleep(2) 
+pyautogui.press("enter")
+
+pyautogui.hotkey('win', 'up') # maximizar o navegador 
+
+for _ in range(5):            # permite aperta tab 5 vezes
+    pyautogui.press("tab") 
+
+time.sleep(2) 
+
+pyautogui.write("09517492936")
+pyautogui.press("tab")
+pyautogui.write("Mayara30171959")
+
+pyautogui.press("enter")
+
+pyautogui.click(957,950, duration=1.5)   # coordenada do OK 
+pyautogui.click(551,636, duration=1.5)   # coordenada do enter 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
